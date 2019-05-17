fcc6664518273a8c40167c326cf9e2cbf4a04d8b
wangjichao <emailtochao@163.com>
2019-05-17 14:29:35 +0800

    解决ButtonRoleGroup传入0或1个Button，children.map报错问题

--- src/components/ButtonRoleGroup/index.js
+++ src/components/ButtonRoleGroup/index.js
@@ -29,7 +29,7 @@ class ButtonRoleGroup extends Component {
         super(props);
         this.state = {
             role: []
-        }
+        };
     }
     /**
      * 可配置的权限参数
@@ -37,7 +37,7 @@ class ButtonRoleGroup extends Component {
     options = {
         method: 'get',
         param: { funcCode: this.props.funcCode, r: Math.random() }
-    }
+    };
     /**
      * 生命周期
      *
@@ -47,16 +47,18 @@ class ButtonRoleGroup extends Component {
         let { onComplete, onError } = this.props;
         let { data } = await request(this.props.url, this.options);
         if (Array.isArray(data)) {
-            this.setState({
-                // role: ['check', 'delete']
-                role: data
-            }, () => {
-                onComplete && onComplete();
-            });
+            this.setState(
+                {
+                    // role: ['check', 'delete']
+                    role: data
+                },
+                () => {
+                    onComplete && onComplete();
+                }
+            );
         } else {
             onError && onError();
         }
-
     }
     /**
      * 判断组件是否为权限内
@@ -64,60 +66,57 @@ class ButtonRoleGroup extends Component {
      * @param {JSX} comp 传入的按钮组件，通过props.role获得
      * @returns boolean
      */
-    hasRoleComp = (comp) => {
+    hasRoleComp = comp => {
         let { role } = this.state;
         let _flag = 'no';
         if (comp.props.role == undefined) {
             _flag = 'normal';
         } else {
-                
             for (let i = 0; i < role.length; i++) {
                 if (role[i] == comp.props.role || comp.props.role == undefined) {
                     _flag = 'normal';
                     break;
-                } 
+                }
             }
         }
 
         return _flag;
-    }
+    };
     render() {
         let { children, className } = this.props;
         return (
             <span className={className}>
-                {
-                    children.map((Comp, key) => {
-                        let attr = {
-                            authority: true,
-                            disabled: false
-                        }
-                        let attrNormal = {
-                            authority: false,
-                            disabled: false
-                        }
-                        switch (this.hasRoleComp(Comp)) {
-                            case 'yes':
-                                attr.disabled = Comp.props.disabled
-                                return <Comp.type key={key} {...Comp.props} {...attr} />
-                            case 'no':
-                                attr.disabled = true;
-                                return <Comp.type key={key} {...Comp.props} {...attr} />
-                            case 'normal':
-                                attrNormal.disabled = Comp.props.disabled
-                                return <Comp.type key={key} {...Comp.props} {...attrNormal} />
-                            default:
-                                return <div>error</div>
-                        }
-                    })
-                }
+                {React.Children.map(this.props.children, (Comp, key) => {
+                    let attr = {
+                        authority: true,
+                        disabled: false
+                    };
+                    let attrNormal = {
+                        authority: false,
+                        disabled: false
+                    };
+                    switch (this.hasRoleComp(Comp)) {
+                        case 'yes':
+                            attr.disabled = Comp.props.disabled;
+                            return <Comp.type key={key} {...Comp.props} {...attr} />;
+                        case 'no':
+                            attr.disabled = true;
+                            return <Comp.type key={key} {...Comp.props} {...attr} />;
+                        case 'normal':
+                            attrNormal.disabled = Comp.props.disabled;
+                            return <Comp.type key={key} {...Comp.props} {...attrNormal} />;
+                        default:
+                            return <div>error</div>;
+                    }
+                })}
             </span>
         );
     }
 }
 ButtonRoleGroup.propTypes = propTypes;
 ButtonRoleGroup.defaultProps = {
-    url: "/wbalone/security/auth",
-    funcCode: "",
-    className: ""
-}
+    url: '/wbalone/security/auth',
+    funcCode: '',
+    className: ''
+};
 export default ButtonRoleGroup;
