ed089a5a654682f929b2b3defbc95c8c1a80b657
WALSIN\ur07492 <barry_lai@walsin.com>
2019-05-23 17:01:29 +0800

    修正錯誤訊息共用方法及全局樣式

--- src/utils/index.js
+++ src/utils/index.js
@@ -27,17 +27,15 @@ export const Info = (msg) => {
  * @param {*} successMsg 成功提示
  */
 export const processData = (response, successMsg) => {
-    let result={};
+    let result = {};
     try {
         if (typeof response != 'object') {
-            Error('数据返回出错：1、请确保服务运行正常；2、请确保您的前端工程代理服务正常；3、请确认您已在本地登录过应用平台');
-            // throw new Error('数据返回出错：1、请确保服务运行正常；2、请确保您的前端工程代理服务正常；3、请确认您已在本地登录过应用平台')
-            return {result:null};
+            // Error('数据返回出错：1、请确保服务运行正常；2、请确保您的前端工程代理服务正常；3、请确认您已在本地登录过应用平台');
+            throw new Error('数据返回出错：1、请确保服务运行正常；2、请确保您的前端工程代理服务正常；3、请确认您已在本地登录过应用平台');
         }
         if (response.status == '401') {
-            Error(`错误:${(response.data.msg)}`);
-            // throw new Error(`错误:${(response.data.msg)}`);
-            return {result:null};
+            // Error(`错误:${(response.data.msg)}`);
+            throw new Error(`错误:${(response.data.msg)}`);
         }
         if (response.status == '200') {
             let data = response.data;
@@ -49,23 +47,21 @@ export const processData = (response, successMsg) => {
                 result.status = repMsg;
                 // 删除成功没有 data 值
                 result.data = data.detailMsg.data || {};
-                return {result};
+                return { result };
             } else if (repMsg == 'fail_field') {
-                Error(`错误:${(data && data.detailMsg && convert(data.detailMsg.msg)) || '数据返回出错'}`);
-                // throw new Error(`错误:${(data && data.detailMsg && convert(data.detailMsg.msg)) || '数据返回出错'}`)
-                return {result:null};
+                // Error(`错误:${(data && data.detailMsg && convert(data.detailMsg.msg)) || '数据返回出错'}`);
+                throw new Error(`错误:${(data && data.detailMsg && convert(data.detailMsg.msg)) || '数据返回出错'}`);
             } else {
-                Error(`错误:${convert(data.message)}`);
-                // throw new Error(`错误:${convert(data.message)}`);
-                return {result:null};
+                // Error(`错误:${convert(data.message)}`);
+                throw new Error(`错误:${convert(data.message)}`);
             }
         } else {
-            Error('请求错误');
-            // throw new Error(`错误:${(response.status)}`);
+            // Error('请求错误');
+			throw new Error(`错误:${(response.status)}`);
         }
 
     } catch (e) {
-        return {result};
+        return { result };
     }
 }
 
